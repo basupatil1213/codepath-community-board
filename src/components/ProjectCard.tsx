44383f6b1d419e1bf2c256656448fc7d493a175c
Basavaraj Patil <patil.ba@northeastern.edu>
2025-06-08 01:11:36 -0400

    update to Project UI

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -4,14 +4,17 @@ type ProjectCardProps = {
     project: Project
 }
 
-const ProjectCard = ({project} : ProjectCardProps) => {
+const ProjectCard = ({ project }: ProjectCardProps) => {
     return (
-        <div className="border-2 m-4 rounded-lg p-4">
+        <div className="border-2 rounded-lg m-4  p-4">
             <p>{project.name}</p>
             <p>{project.description}</p>
-            <p>{project.tech_stack}</p>
+            <div className="flex space-between justify-around">{project.tech_stack.map((skill: string, idx: number) => (
+                    <p key={idx} className="border rounded-lg p-2 bg-orange-500/65 text-[14px] ">{skill}</p>
+                ))}
+            </div>
             <p>{project.owner.name}</p>
-            <a href={project.gihtub_link} target="_blank">github link</a> <br />
+            <a className="text-red" href={project.gihtub_link} target="_blank">github link</a> <br />
             {project.demo_link &&
                 <a href={project.demo_link} target="_blank">Demo link</a>}
         </div>
